566b1c8ee6ac16e3cf625a00222444c64bd51013
Gabriel Rambert <gabriel.rambert@gmail.com>
2012-09-29 03:57:33 +0200

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gbrl::Application.config.secret_token = 'd2faa080b6ffe4640755bf5a461c14e94a5c288a460f59b79dc840d9d821d78fc99459038e00f4425cbcb8d0c1289ff25e2a4240bdf5cfe393c64f17b35b52e1'
